954182faaa3e37d95291b633004a2d0f9b890ee1
Shawn <shawn@mag.cr>
2021-09-20 10:17:14 +0200

    added hero

---
+++ components/Hero/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import _ from './style.module.css'
+
+const Hero = () => {
+  return (
+    <section className={_.hero}>
+      <h1>Marissa Rossouw</h1>
+    </section>
+  )
+}
+
+export default Nav
