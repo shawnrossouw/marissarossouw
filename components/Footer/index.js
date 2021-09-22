623db51d2da09eb3ebd3a3152d8a015335a255ae
Shawn <shawn@mag.cr>
2021-09-22 09:53:34 +0200

    added footer

---
+++ components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import _ from './style.module.css'
+import Link from 'next/link'
+
+const Footer = () => {
+  return (
+    <footer className={_.footer} id='contact'>
+      Footer goes here.
+    </footer>
+  )
+}
+export default Footer
